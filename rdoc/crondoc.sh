f9a9bef85336ead13b211e80d2bef433a91e19d0
Fons Rademakers <Fons.Rademakers@cern.ch>
2014-05-15 17:02:47 +0200

    adjust paths. Will be made generic in next step when all runs fine from repo.

--- rdoc/crondoc.sh
+++ rdoc/crondoc.sh
@@ -4,6 +4,6 @@
 # to set the LD_LIBRARY_PATH needed by some ROOT plugins
 . /user/rdm/.bash_profile
 
-cd /user/rdm/rdoc
+cd /user/rdm/rootspi/rdoc
 ./makedoc.sh master > makedoc-master.log 2>& 1
 ./makedoc.sh 5-34 > makedoc-5-34.log 2>& 1
--- rstats/cronstats.sh
+++ rstats/cronstats.sh
@@ -4,5 +4,5 @@ export ROOTSYS=/user/rdm/root
 export PATH=$ROOTSYS/bin:$PATH
 export LD_LIBRARY_PATH=$ROOTSYS/lib:$LD_LIBRARY_PATH
 
-cd /user/rdm/rstats
+cd /user/rdm/rootspi/rstats
 ./makestats.sh > makestats.log 2>& 1
