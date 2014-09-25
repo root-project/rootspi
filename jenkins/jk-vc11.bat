b45f2009b6276e7de1c4fa541a195d5cca3f9d3a
Pere Mato <pere.mato@cern.ch>
2014-09-25 16:18:15 +0200

    vc11

---
+++ jenkins/jk-vc11.bat
@@ -0,0 +1,19 @@
+@echo off 
+
+echo Execution started: %date% %time%
+
+rem ---Compiler------------------------------------------------------
+call "%VS110COMNTOOLS%vsvars32.bat"
+
+rem ---GSL-----------------------------------------------------------
+rem set GSL_DIR=C:\libs\gsl-1.14
+
+rem ---Options-------------------------------------------------------
+set THIS=%~d0%~p0
+set ExtraCMakeOptions=";-Droofit=ON"
+
+rem ---Run the CTest script------------------------------------------
+ctest -V -S %THIS%root-build.cmake
+
+
+
