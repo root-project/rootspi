99a666cfccebc1188b0dd8d03c3c22fa57d0b7d9
Pere Mato <pere.mato@cern.ch>
2014-10-14 16:25:15 +0200

    Try to fix append

--- jenkins/root-classic.cmake
+++ jenkins/root-classic.cmake
@@ -111,7 +111,7 @@ configure_file(${CTEST_SOURCE_DIRECTORY}/cmake/modules/CTestCustom.cmake ${CTEST
 #####ctest_empty_binary_directory(${CTEST_BINARY_DIRECTORY})
 ctest_start(${CTEST_MODE})
 ctest_update(SOURCE ${CTEST_SOURCE_DIRECTORY})
-ctest_configure(BUILD ${CTEST_BINARY_DIRECTORY} APPEND)
+ctest_configure(BUILD ${CTEST_BINARY_DIRECTORY})
 
 #---Read custom files and generate a note with ignored tests----------------
 ctest_read_custom_files(${CTEST_BINARY_DIRECTORY})
@@ -119,7 +119,8 @@ WRITE_INGNORED_TESTS(${CTEST_BINARY_DIRECTORY}/ignoredtests.txt)
 set(CTEST_NOTES_FILES ${CTEST_BINARY_DIRECTORY}/ignoredtests.txt)
 
 #--------------------------------------------------------------------------
-ctest_build(BUILD ${CTEST_BINARY_DIRECTORY} APPEND)
+ctest_build(BUILD ${CTEST_BINARY_DIRECTORY})
+ctest_submit()
 
 #---Set the environment---------------------------------------------------
 set(ENV{ROOTSYS} ${CTEST_BINARY_DIRECTORY})
