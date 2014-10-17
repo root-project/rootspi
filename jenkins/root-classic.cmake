a20429cebefb7b5042ef440c250059a7671e159e
Patricia Mendez Lorenzo <patricia.mendez@cern.ch>
2014-10-17 17:35:08 +0200

    new root-classic version

--- jenkins/root-classic.cmake
+++ jenkins/root-classic.cmake
@@ -8,12 +8,16 @@ set(CTEST_BUILD_NAME ${CTEST_VERSION}-${tag}${Type$ENV{BUILDTYPE}}-classic)
 set(CTEST_CONFIGURE_COMMAND "${CTEST_SOURCE_DIRECTORY}/configure --all")
 set(CTEST_BUILD_COMMAND "make -s -i -j${ncpu}")
 
+if (EXIST ${CTEST_BINARY_DIRECTORY}){
+file(REMOVE_RECURSE ${CTEST_BINARY_DIRECTORY})
+}
+
 #---Configure tests. Some of them require some files to be copied-----------
 file(MAKE_DIRECTORY ${CTEST_BINARY_DIRECTORY})
 configure_file(${CTEST_SOURCE_DIRECTORY}/cmake/modules/CTestCustom.cmake ${CTEST_BINARY_DIRECTORY} COPYONLY)
 
 #---CTest commands----------------------------------------------------------
-ctest_empty_binary_directory(${CTEST_BINARY_DIRECTORY})
+#ctest_empty_binary_directory(${CTEST_BINARY_DIRECTORY})
 ctest_start(${CTEST_MODE})
 ctest_update(SOURCE ${CTEST_SOURCE_DIRECTORY})
 ctest_configure(BUILD ${CTEST_BINARY_DIRECTORY})
