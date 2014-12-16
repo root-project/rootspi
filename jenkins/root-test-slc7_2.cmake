3ac62e1095b0736d880ee91f5cea98dfa0aba87c
Patricia Mendez Lorenzo <patricia.mendez@cern.ch>
2014-12-16 17:19:22 +0100

    test

--- jenkins/root-test-slc7_2.cmake
+++ jenkins/root-test-slc7_2.cmake
@@ -1,9 +1,9 @@
 #---Common Setting----------------------------------------------------------
 include(${CTEST_SCRIPT_DIRECTORY}/rootCommon.cmake)
-set(CTEST_BUILD_NAME ${CTEST_VERSION}-${tag}${Type$ENV{BUILDTYPE}}-classic)
+set(CTEST_BUILD_NAME ${CTEST_VERSION}-${tag}${Type$ENV{BUILDTYPE}}-slc7)
 
 #---CTest commands----------------------------------------------------------
-ctest_start(${CTEST_MODE} APPEND)
+ctest_start(${CTEST_MODE})
 
 #---Read custom files and generate a note with ignored tests----------------
 ctest_read_custom_files(${CTEST_BINARY_DIRECTORY})
@@ -15,8 +15,8 @@ set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
 file(MAKE_DIRECTORY ${CTEST_BINARY_DIRECTORY}/runtests)
 
 ctest_configure(BUILD   ${CTEST_BINARY_DIRECTORY}/runtests
-                SOURCE  $ENV{ROOTSYS}/tutorials
-                OPTIONS -DCMAKE_MODULE_PATH=$ENV{ROOTSYS}/etc/cmake)
+                SOURCE  $ENV{ROOTSYS}/tutorials)
+
 
 ctest_test(BUILD ${CTEST_BINARY_DIRECTORY}/runtests
            PARALLEL_LEVEL ${ncpu})
