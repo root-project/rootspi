25a1eff420299335f8dd1fcc8697109b01721392
Danilo Piparo <danilo.piparo@cern.ch>
2014-12-17 08:08:02 +0100

    [cc7] Clean and document. Add prototype to run roottest
    at the moment only tutorials are run. This is a good coverage but
    we need to run roottest in addition. The question to be answered
    is how cdash reacts to multiple results uploads.

--- jenkins/root-test-slc7_2.cmake
+++ jenkins/root-test-slc7_2.cmake
@@ -1,12 +1,12 @@
 #---Common Setting----------------------------------------------------------
 include(${CTEST_SCRIPT_DIRECTORY}/rootCommon.cmake)
 set(CTEST_BUILD_NAME ${CTEST_VERSION}-${tag}${Type$ENV{BUILDTYPE}}-slc7)
+set(RTESTSDIR ${CTEST_BINARY_DIRECTORY}/runtests)
 
-if(EXISTS "${CTEST_BINARY_DIRECTORY}/runtests")
-  file(REMOVE_RECURSE ${CTEST_BINARY_DIRECTORY}/runtests)
+#---Clean the directory whe test------------------------------------------------
+if(EXISTS "${RUN_TESTS_DIR}")
+  file(REMOVE_RECURSE ${RUN_TESTS_DIR})
 endif()
-
-
 
 #---CTest commands----------------------------------------------------------
 ctest_start(${CTEST_MODE})
@@ -16,20 +16,32 @@ ctest_read_custom_files(${CTEST_BINARY_DIRECTORY})
 WRITE_INGNORED_TESTS(${CTEST_BINARY_DIRECTORY}/ignoredtests.txt)
 set(CTEST_NOTES_FILES ${CTEST_BINARY_DIRECTORY}/ignoredtests.txt)
 
-
-
 #---Confgure and run the tests--------------------------------------------
 set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
-file(MAKE_DIRECTORY ${CTEST_BINARY_DIRECTORY}/runtests)
+file(MAKE_DIRECTORY ${RUN_TESTS_DIR})
 
-ctest_configure(BUILD   ${CTEST_BINARY_DIRECTORY}/runtests
-                SOURCE  $ENV{ROOTSYS}/tutorials)
-#		 SOURCE  ${CTEST_BINARY_DIRECTORY}/../roottest)
+ctest_configure(BUILD ${RUN_TESTS_DIR}
+                SOURCE $ENV{ROOTSYS}/tutorials)
 
-
-ctest_test(BUILD ${CTEST_BINARY_DIRECTORY}/runtests
+ctest_test(BUILD ${RUN_TESTS_DIR}
            PARALLEL_LEVEL ${ncpu})
 
 ctest_submit(PARTS Test Notes)
+
+# Run roottest also and upload to cdash: EXPERIMENTAL!
+# Will cdash accept 2 results uploads and sum them up?
+# if(EXISTS "${RUN_TESTS_DIR}")
+#   file(REMOVE_RECURSE ${RUN_TESTS_DIR})
+# endif()
+#
+# file(MAKE_DIRECTORY ${RUN_TESTS_DIR})
+# ctest_configure(BUILD   ${RUN_TESTS_DIR}
+#                 SOURCE  ${CTEST_BINARY_DIRECTORY}/../roottest)
+#
+# ctest_test(BUILD ${RUN_TESTS_DIR}
+#            PARALLEL_LEVEL ${ncpu})
+#
+# ctest_submit(PARTS Test Notes)
+
 
 
