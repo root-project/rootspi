bf7838b75d793cf14f087c2352825347db0d482a
Danilo Piparo <danilo.piparo@cern.ch>
2014-12-19 10:29:33 +0100

    Try to run roottest and the tutorials

--- jenkins/root-test-slc7.cmake
+++ jenkins/root-test-slc7.cmake
@@ -24,24 +24,25 @@ ctest_configure(BUILD ${RUN_TESTS_DIR}
                 SOURCE $ENV{ROOTSYS}/tutorials)
 
 ctest_test(BUILD ${RUN_TESTS_DIR}
+           INCLUDE_LABEL tutorial-fit-ErrorIntegral
            PARALLEL_LEVEL ${ncpu})
 
 ctest_submit(PARTS Test Notes)
 
 # Run roottest also and upload to cdash: EXPERIMENTAL!
 # Will cdash accept 2 results uploads and sum them up?
-# if(EXISTS "${RUN_TESTS_DIR}")
-#   file(REMOVE_RECURSE ${RUN_TESTS_DIR})
-# endif()
-#
-# file(MAKE_DIRECTORY ${RUN_TESTS_DIR})
-# ctest_configure(BUILD   ${RUN_TESTS_DIR}
-#                 SOURCE  ${CTEST_BINARY_DIRECTORY}/../roottest)
-#
-# ctest_test(BUILD ${RUN_TESTS_DIR}
-#            PARALLEL_LEVEL ${ncpu})
-#
-# ctest_submit(PARTS Test Notes)
+if(EXISTS "${RUN_TESTS_DIR}")
+  file(REMOVE_RECURSE ${RUN_TESTS_DIR})
+endif()
+
+file(MAKE_DIRECTORY ${RUN_TESTS_DIR})
+ctest_configure(BUILD   ${RUN_TESTS_DIR}
+                SOURCE  ${CTEST_BINARY_DIRECTORY}/../roottest)
+
+ctest_test(BUILD ${RUN_TESTS_DIR}
+           PARALLEL_LEVEL ${ncpu})
+
+ctest_submit(PARTS Test Notes)
 
 
 
