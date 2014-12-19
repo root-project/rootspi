9f080efaf0d7c4307202d905596ca305ca9bd325
Danilo Piparo <danilo.piparo@cern.ch>
2014-12-19 10:26:40 +0100

    Clean up of cc7 scripts

--- jenkins/root-test-slc7.cmake
+++ jenkins/root-test-slc7.cmake
@@ -1,39 +1,47 @@
 #---Common Setting----------------------------------------------------------
 include(${CTEST_SCRIPT_DIRECTORY}/rootCommon.cmake)
 set(CTEST_BUILD_NAME ${CTEST_VERSION}-${tag}${Type$ENV{BUILDTYPE}}-slc7)
+set(RUN_TESTS_DIR ${CTEST_BINARY_DIRECTORY}/runtests)
+
+#---Clean the directory whe test------------------------------------------------
+if(EXISTS "${RUN_TESTS_DIR}")
+  file(REMOVE_RECURSE ${RUN_TESTS_DIR}) 
+endif()
 
 #---CTest commands----------------------------------------------------------
-ctest_start(${CTEST_MODE} APPEND)
+ctest_start(${CTEST_MODE})
 
 #---Read custom files and generate a note with ignored tests----------------
 ctest_read_custom_files(${CTEST_BINARY_DIRECTORY})
 WRITE_INGNORED_TESTS(${CTEST_BINARY_DIRECTORY}/ignoredtests.txt)
 set(CTEST_NOTES_FILES ${CTEST_BINARY_DIRECTORY}/ignoredtests.txt)
 
-#---Set the environment---------------------------------------------------
-#set(ENV{ROOTSYS} ${CTEST_BINARY_DIRECTORY})
-#set(ENV{PATH} ${CTEST_BINARY_DIRECTORY}/bin:$ENV{PATH})
-#if(APPLE)
-#  set(ENV{DYLD_LIBRARY_PATH} ${CTEST_BINARY_DIRECTORY}/lib:$ENV{DYLD_LIBRARY_PATH})
-#elseif(UNIX)
-#  set(ENV{LD_LIBRARY_PATH} ${CTEST_BINARY_DIRECTORY}/lib:$ENV{LD_LIBRARY_PATH})
-#endif()
-#set(ENV{PYTHONPATH} ${CTEST_BINARY_DIRECTORY}/lib:$ENV{PAYTHONPATH})
-
 #---Confgure and run the tests--------------------------------------------
 set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
-file(MAKE_DIRECTORY ${CTEST_BINARY_DIRECTORY}/runtests)
+file(MAKE_DIRECTORY ${RUN_TESTS_DIR})
 
-message("hhhhhhhhhhhhhhh: ${CTEST_BINARY_DIRECTORY}")
+ctest_configure(BUILD ${RUN_TESTS_DIR}
+                SOURCE $ENV{ROOTSYS}/tutorials)
 
-ctest_configure(BUILD    ${CTEST_BINARY_DIRECTORY}/runtests
-                SOURCE   ${CTEST_BINARY_DIRECTORY}/../roottest
-#		 SOURCE  ${CTEST_BINARY_DIRECTORY}/tutorials
-                OPTIONS -DCMAKE_MODULE_PATH=$ENV{ROOTSYS}/etc/cmake)
-
-ctest_test(BUILD ${CTEST_BINARY_DIRECTORY}/runtests
+ctest_test(BUILD ${RUN_TESTS_DIR}
            PARALLEL_LEVEL ${ncpu})
 
 ctest_submit(PARTS Test Notes)
+
+# Run roottest also and upload to cdash: EXPERIMENTAL!
+# Will cdash accept 2 results uploads and sum them up?
+# if(EXISTS "${RUN_TESTS_DIR}")
+#   file(REMOVE_RECURSE ${RUN_TESTS_DIR})
+# endif()
+#
+# file(MAKE_DIRECTORY ${RUN_TESTS_DIR})
+# ctest_configure(BUILD   ${RUN_TESTS_DIR}
+#                 SOURCE  ${CTEST_BINARY_DIRECTORY}/../roottest)
+#
+# ctest_test(BUILD ${RUN_TESTS_DIR}
+#            PARALLEL_LEVEL ${ncpu})
+#
+# ctest_submit(PARTS Test Notes)
+
 
 
