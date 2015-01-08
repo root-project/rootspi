dcdc935ce5a3a1ee774354e5ceaa7c5ed5cee391
Danilo Piparo <danilo.piparo@cern.ch>
2015-01-08 14:16:29 +0100

    Exclude3 tutorials depending on rootfile not distributed w/ nightlies

--- jenkins/root-test-slc7.cmake
+++ jenkins/root-test-slc7.cmake
@@ -51,7 +51,9 @@ ctest_configure(BUILD   ${RUN_TESTS_DIR_ROOTTEST}
 
 # Exclsion regex:
 # - MakeIndex: THtml has no sources at disposal and fails
-set(EXCLUSIONREGEX html-runMakeIndex)
+# - tutorial-tree-staff,tutorial-tree-cernstaff,tutorial-hist-hbars: input
+#    rootfile not distributed with the externals
+set(EXCLUSIONREGEX "html-runMakeIndex|tutorial-tree-staff|tutorial-tree-cernstaff|tutorial-hist-hbars")
 
 ctest_test(BUILD ${RUN_TESTS_DIR_ROOTTEST}
            APPEND
