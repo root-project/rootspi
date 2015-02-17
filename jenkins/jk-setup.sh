9c62720a6086849666accd244aca96cb3e4bbca8
Patricia Mendez <patricia.mendez@cern.ch>
2015-02-17 17:19:34 +0100

    fixing jk-setup

--- jenkins/jk-setup.sh
+++ jenkins/jk-setup.sh
@@ -39,7 +39,7 @@ then
   export CXX=`which g++`
   export CC=`which gcc`
 
-  export ExtraCMakeOptions="-Dchirp=OFF -Dhdfs=OFF -Dbonjour=OFF -Dfail-on-missing=ON -DTCMALLOC_DIR=/afs/cern.ch/sw/lcg/app/releases/ROOT-externals/${EXTERNALS}/${ARCH}-${LABEL}-${COMPILER}-${BUILD_TYPE}/tcmalloc  ${ExtraCMakeOptions}"
+  export ExtraCMakeOptions="-Dchirp=OFF -Dhdfs=OFF -Dbonjour=OFF -Dfail-on-missing=ON ${ExtraCMakeOptions}"
 
 elif [[ $COMPILER == *clang* ]]
 then
