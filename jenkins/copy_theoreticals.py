babdb593fe2094a195ba70f8580e7b9ff7243079
Patricia Mendez <patricia.mendez@cern.ch>
2015-02-26 16:53:37 +0100

    new script for the copy of TH packages

---
+++ jenkins/copy_theoreticals.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+
+import sys, getopt, fnmatch, os, subprocess, platform, string, re
+global arch, system
+
+arch = platform.machine()
+system = platform.system()
+
+# --------------------- Setting command lines options 
+def main(argv):
+   global compiler, build_type, op_sys, external, rootDir, targetDir, packs
+
+   build_type = ''
+   op_sys = ''
+   external = ''
+   packs = ''
+   compiler = ''
+
+   opts, args = getopt.getopt(argv,"hc:b:o:v:t:")
+   for opt, arg in opts:
+      if opt == '-h':
+         print 'setup.py -c <compiler> -b <build_type> -o <operating_system> -v <external> -t <list of packages>'
+         sys.exit()
+      elif opt in ("-c"):
+         compiler = arg
+
+      elif opt in ("-b"):
+         build = arg
+
+      elif opt in ("-o"):
+         op_sys = arg
+
+      elif opt in ("-v"):
+         external = arg
+
+      elif opt in ("-t"):
+         packs = arg
+
+   if build == 'Release' : build_type = 'opt'
+   elif build == 'Debug' : build_type = 'dbg'
+   elif build == 'Optimized' : build_type = 'opt'
+   else : build_type = 'unk'
+
+   rootDir = "/afs/cern.ch/user/s/sftnight/tmp/"+external 
+   targetDir = "/afs/cern.ch/sw/lcg/hepsoft/0.9b/"+arch+"-"+op_sys+"-"+compiler+"-"+build_type+"/"
+# --------------------- Setting the targets
+def targets():
+
+   words = packs.split()
+   return words;
+
+# --------------------- Setting names of the main tree directory 
+
+def directories():
+   dir_hash = []
+
+   for dirs in os.listdir(rootDir):
+      if os.path.isfile(dirs):
+         pass
+      else:
+         dir_hash.append(dirs)
+   return dir_hash;      
+
+# --------------------- Setting paths  
+def directory_names():
+
+   dir_hash = directories()
+   pkg_hash = targets()
+
+   for j in range(0,len(pkg_hash)):
+
+      for i in dir_hash:
+
+         fullpath = rootDir+"/"+i
+         if pkg_hash[j] in fullpath:
+            for dirName, subdirList, fileList in os.walk(fullpath):   
+
+               for name in subdirList:
+                  if ((name.find(compiler) != -1) and (name.find(build_type) != -1) and (name.find(op_sys) != -1)):
+                     directory =  os.path.join(dirName, name)
+                     if "castor" in directory:
+                        directory = directory + '/usr'
+                     cmd = "cp -r %s/* %s"%(directory,targetDir)
+
+                     os.system(cmd)
+
+# -------------------------------------------------------------------------------------------------
+
+if __name__ == "__main__":
+
+   main(sys.argv[1:])
+
+   targets()
+   directory_names()
+
+
