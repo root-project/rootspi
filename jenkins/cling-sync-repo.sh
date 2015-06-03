3695bc3c11288c0ee9230636e13dba36a90be8fc
Axel Naumann <Axel.Naumann@cern.ch>
2015-06-03 10:38:54 +0200

    Use sub-project to check out https://cling.git.

--- jenkins/cling-sync-repo.sh
+++ jenkins/cling-sync-repo.sh
@@ -6,10 +6,12 @@ set -x
 #  clean: remove repos
 #  fromscratch: clean, then build
 
+clingRepo="`dirname $WORKSPACE`/cling-clone-repo"
+
 function clean {
   # remove checked-out repos
   rm -rf root
-  rm -rf cling
+  rm -rf $clingRepo
 }
 
 mode="x$1"
@@ -37,11 +39,12 @@ fi
 [ "$mode" = "xfromscratch" ] && clean
 
 # Clone / update cling
-[ -d cling ] || git clone https://root.cern.ch/git/cling.git
-cd cling
-git pull
-git reset --hard
-cd ..
+# Done by cling-clone-repo
+#[ -d cling ] || git clone https://root.cern.ch/git/cling.git
+#cd cling
+#git pull
+#git reset --hard
+#cd ..
 
 # What was the last commit in root.git's interpreter/cling?
 rootCommit=`cd root&& git rev-list -n 1 HEAD -- interpreter/cling`
@@ -52,7 +55,7 @@ fi
 echo "Most recent commit in ROOT/interpreter/cling: $rootCommit"
 
 # What was the most recent ROOT commit merged into cling?
-clingTag=`cd cling && git tag -l '__internal-root-*'`
+clingTag=`cd $clingRepo && git tag -l '__internal-root-*'`
 if [ "x$clingTag" = "x" ]; then
   echo 'Cannot extract most recent merge tag!' >&2
   exit
@@ -66,19 +69,18 @@ fi
 startCommit="${clingTag/__internal-root-/}"
 echo "Applying patches after $startCommit and up to $rootCommit"
 
-patch="root-to-cling-$startCommit-$rootCommit.patch";
+patch="$WORKSPACE/root-to-cling-$startCommit-$rootCommit.patch";
 
 # extract the patch
 cd root
-git format-patch --no-stat --find-renames --find-copies --stdout --keep-subject $startCommit..$rootCommit -- interpreter/cling/ > ../$patch
+git format-patch --no-stat --find-renames --find-copies --stdout --keep-subject $startCommit..$rootCommit -- interpreter/cling/ > $patch
 
 # For better logs:
-cd ..
 cat $patch
 
 # apply the patch
-cd cling
-git am -p3 ../$patch
+cd $clingRepo
+git am -p3 $patch
 git push origin master
 newTag="__internal-root-$rootCommit"
 git tag $newTag
@@ -87,5 +89,4 @@ git tag -d $clingTag
 git push origin :refs/tags/$clingTag
 
 # clean up
-cd ..
 rm $patch
