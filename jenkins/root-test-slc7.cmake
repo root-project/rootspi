b7d08af19734b0228fd760e946d43cc13e04652c
Pere Mato <pere.mato@cern.ch>
2015-07-17 11:57:51 +0200

    Needed to clean the roottest sources

--- jenkins/root-test-slc7.cmake
+++ jenkins/root-test-slc7.cmake
@@ -4,36 +4,43 @@ include(${CTEST_SCRIPT_DIRECTORY}/rootCommon.cmake)
 set(CTEST_CHECKOUT_COMMAND "cmake -E chdir roottest ${CTEST_GIT_COMMAND} checkout -f $ENV{GIT_PREVIOUS_COMMIT}")
 set(CTEST_GIT_UPDATE_CUSTOM  ${CTEST_GIT_COMMAND} checkout -f $ENV{GIT_COMMIT})
 
+get_filename_component(CTEST_SOURCE_DIRECTORY roottest ABSOLUTE)
+get_filename_component(CTEST_BINARY_DIRECTORY . ABSOLUTE)
+
 set(CTEST_BUILD_NAME ${CTEST_VERSION}-${tag}${Type$ENV{BUILDTYPE}}-slc7)
-set(RUN_TESTS_DIR ${CTEST_BINARY_DIRECTORY}/runtests)
-set(RUN_TESTS_DIR_ROOTTEST ${CTEST_BINARY_DIRECTORY}/runtests-roottest)
+set(RUN_TESTS_DIR_TUTORIALS ${CTEST_BINARY_DIRECTORY}/runtests-tutorials)
+set(RUN_TESTS_DIR_ROOTTEST  ${CTEST_BINARY_DIRECTORY}/runtests-roottest)
 set(TESTS_RESULTS_DIR ${CTEST_BINARY_DIRECTORY}/Testing)
 
 #---Clean the directory where the tests results are-----------------------------
 if(EXISTS "${TESTS_RESULTS_DIR}")
    file(REMOVE_RECURSE ${TESTS_RESULTS_DIR})
 endif()
+
 if(EXISTS "${RUN_TESTS_DIR_ROOTTEST}")
   file(REMOVE_RECURSE ${RUN_TESTS_DIR_ROOTTEST})
 endif()
 
+#---Clean roottest sources------------------------------------------------------
+execute_process(COMMAND cmake -E chdir roottest ${CTEST_GIT_COMMAND} clean -xfdq)
+
 #---Clean the directory whe test------------------------------------------------
-if(EXISTS "${RUN_TESTS_DIR}")
-  file(REMOVE_RECURSE ${RUN_TESTS_DIR})
+if(EXISTS "${RUN_TESTS_DIR_TUTORIALS}")
+  file(REMOVE_RECURSE ${RUN_TESTS_DIR_TUTORIALS})
 endif()
 
 #---CTest commands----------------------------------------------------------
-ctest_start(${CTEST_MODE} roottest)
+ctest_start(${CTEST_MODE})
 ctest_update()
 
 #---For the tutorials--------------------------------------------
-file(MAKE_DIRECTORY ${RUN_TESTS_DIR})
-ctest_configure(BUILD ${RUN_TESTS_DIR}
+file(MAKE_DIRECTORY ${RUN_TESTS_DIR_TUTORIALS})
+ctest_configure(BUILD ${RUN_TESTS_DIR_TUTORIALS}
                 SOURCE $ENV{ROOTSYS}/tutorials)
 
-ctest_build(BUILD ${RUN_TESTS_DIR})
+ctest_build(BUILD ${RUN_TESTS_DIR_TUTORIALS})
 
-ctest_test(BUILD ${RUN_TESTS_DIR}
+ctest_test(BUILD ${RUN_TESTS_DIR_TUTORIALS}
            PARALLEL_LEVEL ${ncpu})
 
 ctest_submit(PARTS Update Configure Build Test Notes)
@@ -46,7 +53,7 @@ ctest_configure(BUILD   ${RUN_TESTS_DIR_ROOTTEST}
 
 list(APPEND CTEST_CUSTOM_TESTS_IGNORE
             roottest-cling-parsing-semicolon
-            html-runMakeIndex
+            roottest-root-html-runMakeIndex
             tutorial-tree-staff
             tutorial-tree-cernstaff
             tutorial-hist-hbars)
