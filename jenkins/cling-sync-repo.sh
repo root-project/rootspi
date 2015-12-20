12208d35c8cb886eab3899224a95262d28322112
Axel Naumann <Axel.Naumann@cern.ch>
2015-12-20 23:58:37 +0100

    Only use last = latest tag.

--- jenkins/cling-sync-repo.sh
+++ jenkins/cling-sync-repo.sh
@@ -19,7 +19,7 @@ fi
 echo "Most recent commit in ROOT/interpreter/cling: $rootCommit"
 
 # What was the most recent ROOT commit merged into cling?
-clingTag=`cd $clingRepo && git tag -l '__internal-root-*'`
+clingTag=`cd $clingRepo && ( git tag -l '__internal-root-*' | tail -n 1 )`
 if [ "x$clingTag" = "x" ]; then
   echo 'Cannot extract most recent merge tag!' >&2
   exit 1
