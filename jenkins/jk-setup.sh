b11a27da36249eebecd49ffcda20fb5cb49d2aa0
Pere Mato <pere.mato@cern.ch>
2016-02-29 13:54:09 +0100

    Change the order to be able to overwrite with the proper compiler

--- jenkins/jk-setup.sh
+++ jenkins/jk-setup.sh
@@ -35,56 +35,6 @@ else
   export EXTERNALDIR=$HOME/ROOT-externals
 fi
 
-if [[ $COMPILER == *gcc* ]]; then
-
-  export ExtraCMakeOptions="-Dchirp=OFF -Dhdfs=OFF -Dbonjour=OFF ${ExtraCMakeOptions}"
-  if [ $ARCH != i686 ]; then
-    export ExtraCMakeOptions="-Dfail-on-missing=ON -Dbuiltin_lzma=ON ${ExtraCMakeOptions}"
-  fi 
-
-elif [[ $COMPILER == *clang* ]]; then
-  clang34version=3.4
-  clang35version=3.5
-  clang36version=3.6
-  COMPILERversion=${COMPILER}version
-  clang34gcc=48
-  clang35gcc=49
-  clang36gcc=49
-  GCCversion=${COMPILER}gcc
-
-  . /afs/cern.ch/sw/lcg/external/llvm/${!COMPILERversion}/${ARCH}-slc6/setup.sh
-  export CC=`which clang`
-  export CXX=`which clang++`
-
-  export ExtraCMakeOptions="${ExtraCMakeOptions} -Dfortran=OFF -Dgcctoolchain=$(dirname $(dirname `which gcc`))"
-
-elif [[ $COMPILER == *native* ]]; then
-  if [[ $LABEL == *mac* ]] ; then
-    export FC=`which gfortran`
-  else
-    export ExtraCMakeOptions="-Dfortran=OFF ${ExtraCMakeOptions}"
-  fi
-elif [[ $COMPILER == *icc* ]]; then
-  iccyear=2013
-  icc14year=2013
-  icc15year=2015
-  COMPILERyear=${COMPILER}year
-
-  iccgcc=4.8
-  icc14gcc=4.8
-  icc15gcc=4.9
-  GCCversion=${COMPILER}gcc
-
-  . /afs/cern.ch/sw/lcg/contrib/gcc/${!GCCversion}/${ARCH}-slc6/setup.sh
-  . /afs/cern.ch/sw/IntelSoftware/linux/setup.sh
-  . /afs/cern.ch/sw/IntelSoftware/linux/${ARCH}/xe${!COMPILERyear}/bin/ifortvars.sh intel64
-  . /afs/cern.ch/sw/IntelSoftware/linux/${ARCH}/xe${!COMPILERyear}/bin/iccvars.sh intel64
-  export CC=icc
-  export CXX=icc
-  export FC=ifort
-  export ExtraCMakeOptions="${ExtraCMakeOptions} -Dvc=OFF"
-fi
-
 # Setup all the externals now-----------------------------------------------------
 PLATFORM=`$THIS/getPlatform.py`
 COMPATIBLE=`$THIS/getCompatiblePlatform.py $PLATFORM`
@@ -97,3 +47,48 @@ elif [[ $PLATFORM == *slc6* ]]; then
 else
   echo "No externals for $PLATFORM in $EXTERNALDIR/$EXTERNALS"
 fi
+
+# The final compiler may not yet be totally setup-------------------------------------
+if [[ $COMPILER == *gcc* ]]; then
+  export ExtraCMakeOptions="-Dchirp=OFF -Dhdfs=OFF -Dbonjour=OFF ${ExtraCMakeOptions}"
+  if [ $ARCH != i686 ]; then
+    export ExtraCMakeOptions="-Dfail-on-missing=ON -Dbuiltin_lzma=ON ${ExtraCMakeOptions}"
+  fi 
+elif [[ $COMPILER == *clang* ]]; then
+  clang34version=3.4
+  clang35version=3.5
+  clang36version=3.6
+  COMPILERversion=${COMPILER}version
+  clang34gcc=48
+  clang35gcc=49
+  clang36gcc=49
+  GCCversion=${COMPILER}gcc
+  . /afs/cern.ch/sw/lcg/external/llvm/${!COMPILERversion}/${ARCH}-slc6/setup.sh
+  export CC=`which clang`
+  export CXX=`which clang++`
+  export ExtraCMakeOptions="${ExtraCMakeOptions} -Dfortran=OFF -Dgcctoolchain=$(dirname $(dirname `which gcc`))"
+elif [[ $COMPILER == *native* ]]; then
+  if [[ $LABEL == *mac* ]] ; then
+    export FC=`which gfortran`
+  else
+    export ExtraCMakeOptions="-Dfortran=OFF ${ExtraCMakeOptions}"
+  fi
+elif [[ $COMPILER == *icc* ]]; then
+  iccyear=2013
+  icc14year=2013
+  icc15year=2015
+  COMPILERyear=${COMPILER}year
+  iccgcc=4.8
+  icc14gcc=4.8
+  icc15gcc=4.9
+  GCCversion=${COMPILER}gcc
+  . /afs/cern.ch/sw/lcg/contrib/gcc/${!GCCversion}/${ARCH}-slc6/setup.sh
+  . /afs/cern.ch/sw/IntelSoftware/linux/setup.sh
+  . /afs/cern.ch/sw/IntelSoftware/linux/${ARCH}/xe${!COMPILERyear}/bin/ifortvars.sh intel64
+  . /afs/cern.ch/sw/IntelSoftware/linux/${ARCH}/xe${!COMPILERyear}/bin/iccvars.sh intel64
+  export CC=icc
+  export CXX=icc
+  export FC=ifort
+  export ExtraCMakeOptions="${ExtraCMakeOptions} -Dvc=OFF"
+fi
+
