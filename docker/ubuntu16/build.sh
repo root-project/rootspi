ed774c272f8f8a6f282339a301c304a7e9560b79
Martin Storo Nyflott <martin.storo.nyflott@cern.ch>
2017-08-14 11:29:06 +0200

    Add support for Ubuntu 16.04 Docker distribution

---
+++ docker/ubuntu16/build.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+set -x
+set -e
+
+if [ $# -ge 4 ]; then
+  LABEL=$1 ; shift
+  COMPILER=$1 ; shift
+  BUILDTYPE=$1 ; shift
+  BRANCH=$1 ; shift
+else
+  echo "$0: expecting 4 arguments [LABEL] [COMPILER] [BUILDTYPE] [BRANCH]"
+  exit 1
+fi
+
+# Setup build env
+cd /root-build
+
+export CCACHE_DIR=/ccache
+export JENKINS_HOME=/tmp
+export EXTERNALS=docker
+
+export MODE=experimental
+export ExtraCMakeOptions='-Dccache=ON -DCMAKE_INSTALL_PREFIX=/usr/local -Dgnuinstall=ON'
+
+export LABEL
+export COMPILER
+export BUILDTYPE
+
+# Build
+rootspi/jenkins/jk-all build
+# Install
+cmake -P build/cmake_install.cmake
+# Test
+rootspi/jenkins/jk-all test
+# Stash test reports and cleanup
+rm -rf Testing
+cp -r build/Testing Testing
+rm -rf build
