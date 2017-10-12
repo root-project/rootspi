20d7770b892f1674162ec59e8969c7e74d183fca
Bertrand Bellenot <bertrand.bellenot@cern.ch>
2017-10-12 21:11:27 +0200

    clean-up

--- jenkins/jk-all.bat
+++ jenkins/jk-all.bat
@@ -16,7 +16,6 @@ rem set GSL_DIR=C:\libs\gsl-1.14
 rem ---Options-------------------------------------------------------
 set THIS=%~d0%~p0
 if %COMPILER% == vc15 (
-  rem should be: ";-Dall=OFF;-Dcxx11=OFF;-Dcxx14=ON;-Dtmva=OFF;-Dimt=OFF;-Dbuiltin_tbb=OFF;-Dtesting=OFF;-Droottest=OFF"
   set "ExtraCMakeOptions=-Dall=OFF -Dcxx11=OFF -Dcxx14=ON -Dtmva=OFF -Dimt=OFF -Dbuiltin_tbb=OFF -Dtesting=OFF -Droottest=OFF"
 ) else (
   set ExtraCMakeOptions=";-Droofit=ON"
