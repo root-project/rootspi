f8b3d501e1fd7a11eeb8719d0a16de81566be406
Bertrand Bellenot <bertrand.bellenot@cern.ch>
2017-10-13 15:50:05 +0200

    Disable ROOT 7 for now (__PRETTY_FUNCTION__ is not defined)

--- jenkins/jk-all.bat
+++ jenkins/jk-all.bat
@@ -16,7 +16,7 @@ rem set GSL_DIR=C:\libs\gsl-1.14
 rem ---Options-------------------------------------------------------
 set THIS=%~d0%~p0
 if %COMPILER% == vc15 (
-  set "ExtraCMakeOptions=-Wno-dev -Dall=OFF -Dcxx11=OFF -Dcxx14=ON -Dtmva=OFF -Dimt=OFF -Dbuiltin_tbb=OFF -Dtesting=OFF -Droottest=OFF"
+  set "ExtraCMakeOptions=-Wno-dev -Dall=OFF -Droot7=OFF -Dcxx11=OFF -Dcxx14=ON -Dtmva=OFF -Dimt=OFF -Dbuiltin_tbb=OFF -Dtesting=OFF -Droottest=OFF"
 ) else (
   set ExtraCMakeOptions=";-Droofit=ON"
 )
