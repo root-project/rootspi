7cc18b8c7c3cfca22861a554d06e77bc4541fe25
Bertrand Bellenot <bertrand.bellenot@cern.ch>
2017-10-25 09:09:01 +0200

    Add batch equivalent of pr-reset.sh and use it in root-build.cmake on Windows

---
+++ jenkins/pr-reset.bat
@@ -0,0 +1,9 @@
+@echo off
+
+rem Resets the pull request repository to head on requested merging branch
+rem First parameter: Cloned ROOT repo location
+rem Second parameter: Location of git
+
+cd %1
+%2 fetch $GIT_URL +refs/heads/*:refs/remotes/origin/*
+%2 checkout -f origin/$ghprbTargetBranch\
