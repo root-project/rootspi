3007b5979535bc82aec184e720f97e4d8b8f3994
Guilherme Amadio <amadio@cern.ch>
2018-07-31 11:47:00 +0200

    Revert "Print variables from GitHub in pr-reset.sh"
    This reverts commit e2d616ae0cc91db43201c68f4f1491666043016a.

--- jenkins/pr-reset.sh
+++ jenkins/pr-reset.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-printenv | grep GIT_
-printenv | grep ghprb
-
 # Resets the pull request repository to head on requested merging branch
 # First parameter: Cloned ROOT repo location
 # Second parameter: Location of git
 
 cd $1
 $2 fetch $GIT_URL +refs/heads/*:refs/remotes/origin/*
-$2 checkout -f origin/$ghprbTargetBranch
+$2 checkout -f origin/$ghprbTargetBranch\
