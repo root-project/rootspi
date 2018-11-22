1d1fd22ba6d416bb52d7f9218e09b7fced2f765e
Oksana Shadura <oksana.shadura@cern.ch>
2018-11-22 14:43:46 +0100

    Adding old setup scripts, temporary until infrastructure will be stable

---
+++ jenkins/rootbench/jk-setup-rootbench.sh
@@ -0,0 +1,195 @@
+#!/bin/bash -x
+
+export LC_CTYPE=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+
+THIS=$(dirname ${BASH_SOURCE[0]})
+
+# first arguments is the source directory
+if [ $# -ge 4 ]; then
+  LABEL=$1 ; shift
+  COMPILER=$1 ; shift
+  BUILDTYPE=$1 ; shift
+  EXTERNALS=$1 ; shift
+else
+  echo "$0: expecting 4 arguments [LABEL]  [COMPILER] [BUILDTYPE] [EXTERNALS]"
+  exit 1
+fi
+
+export BUILDTYPE
+export COMPILER
+
+PLATFORM=`$THIS/../getPlatform.py`
+ARCH=$(uname -m)
+
+# Adjust LABEL and enviroment PATH depending on the OS
+if [[ $PLATFORM == *slc6* ]]; then
+  LABEL=slc6
+elif [[ $PLATFORM == *centos7* ]]; then
+  LABEL=centos7
+elif [[ $PLATFORM == *mac* ]]; then
+  export PATH=/usr/local/bin:${PATH}
+elif [[ $PLATFORM == *fedora* ]]; then
+  export PATH=/usr/local/bin:${PATH}
+fi
+
+# Setup all the externals now-----------------------------------------------------
+PLATFORM=`$THIS/../getPlatform.py`
+COMPATIBLE=`$THIS/../getCompatiblePlatform.py $PLATFORM`
+
+if [[ $COMPILER == gcc73 ]]; then
+  PLATFORM=${PLATFORM/gcc73/gcc7}
+  COMPATIBLE=${COMPATIBLE/gcc73/gcc7}
+fi
+
+if [[ $(uname -s) == Linux ]]; then
+  LCG_EXTERNALS=/cvmfs/sft.cern.ch/lcg/views/$EXTERNALS
+  if [[ -e $LCG_EXTERNALS/$PLATFORM/setup.sh ]]; then
+    source $LCG_EXTERNALS/$PLATFORM/setup.sh
+  elif [[ -e $LCG_EXTERNALS/$COMPATIBLE/setup.sh ]]; then
+    source $LCG_EXTERNALS/$COMPATIBLE/setup.sh
+  elif [[ $PLATFORM == *slc6* || $PLATFORM == *centos7* ]]; then
+    export PATH=/cvmfs/sft.cern.ch/lcg/contrib/CMake/3.6.0/Linux-$ARCH/bin:${PATH}
+  else
+    echo "No $EXTERNALS externals found for $PLATFORM"
+  fi
+fi
+
+# The final compiler may not yet be totally setup-------------------------------------
+if [[ $COMPILER == gcc* ]]; then
+  export ExtraCMakeOptions="-Dchirp=OFF -Dhdfs=OFF -Dbonjour=OFF ${ExtraCMakeOptions}"
+  if [ $ARCH != i686 ]; then
+    export ExtraCMakeOptions="-Dbuiltin_lzma=ON ${ExtraCMakeOptions}"
+  fi
+
+  if [[ $COMPILER == gcc73 ]]; then
+    source /cvmfs/sft.cern.ch/lcg/contrib/gcc/7.3.0binutils/${COMPATIBLE}/setup.sh || exit 1
+    export PATH=/cvmfs/sft.cern.ch/lcg/contrib/gcc/7.3.0binutils/${COMPATIBLE}/bin:$PATH
+  fi
+elif [[ $COMPILER == clang_gcc* ]]; then
+  # We are cross compiling. We use clang as a compiler with libstdc++.
+  # Get the gcc version. First parameter is a zero-based offset and the second is the length.
+  GCCcompiler=${COMPILER:6:5}
+  GCCversion=${COMPILER:9:2}
+  GCCversion="${GCCversion:0:1}.${GCCversion:1:1}"
+  . /cvmfs/sft.cern.ch/lcg/contrib/gcc/${GCCversion}/${ARCH}-${LABEL}/setup.sh
+
+  # FIXME: Horrible hack for finding the lcg releases on mac1012. They are under x86_64-mac1012-clang81-opt
+  # We shouldn't use the compiler version to denote the toolchain which was used to compile the releases
+  # but the version of the standard library. I.e. libcxx or libstdc++.
+  if [[ $PLATFORM == *mac1012* ]]; then
+      GCCcompiler="clang81"
+  else
+      . /cvmfs/sft.cern.ch/lcg/contrib/gcc/${GCCversion}/${ARCH}-${LABEL}/setup.sh
+  fi
+
+  export PATH=/cvmfs/sft.cern.ch/lcg/contrib/llvm/latest/${ARCH}-${LABEL}-${GCCcompiler}-opt/bin/:$PATH
+  export CC=`which clang`
+  export CXX=`which clang++`
+  export ExtraCMakeOptions="${ExtraCMakeOptions} -Dfortran=OFF -Dhdfs=OFF"
+  # On slc we want to compile with a more 'standard' toolchain.
+  if [[ $PLATFORM == *slc* ]] || [[ $LABEL == *centos7* ]] ; then
+    export ExtraCMakeOptions="${ExtraCMakeOptions} -Dgcctoolchain=$(dirname $(dirname `which gcc`))"
+  fi
+
+elif [[ $COMPILER == *clang* ]]; then
+  clang34version=3.4
+  clang35version=3.5
+  clang36version=3.6
+  clang37version=3.7
+  clang38version=3.8
+  clang39version=3.9
+  clang50version=5.0
+  clang500version=5.0.0
+  clang501version=5.0.1
+  clang600version=6.0.0binutils
+  clang501binutilsversion=5.0.1binutils
+  clang600binutilsversion=6.0.0binutils
+  COMPILERversion=${COMPILER}version
+  clang34gcc=4.8
+  clang35gcc=4.9
+  clang36gcc=4.9
+  clang37gcc=4.9
+  clang38gcc=4.9
+  clang39gcc=6.2
+  clang50gcc=6.2
+  clang500gcc=6.2
+  clang501gcc=6.2
+  clang501binutilsgcc=6.2
+  clang600binutilsgcc=6.2
+  clang600gcc=6.2
+  GCCversion=${COMPILER}gcc
+  . /cvmfs/sft.cern.ch/lcg/contrib/llvm/${!COMPILERversion}/${COMPATIBLE}/setup.sh
+
+  export CC=`which clang`
+  export CXX=`which clang++`
+  export ExtraCMakeOptions="${ExtraCMakeOptions} -Dfortran=OFF -Doracle=OFF -Dhdfs=OFF"
+
+  # On slc we want to compile with a more 'standard' toolchain.
+  if [[ $PLATFORM == *slc* ]] || [[ $LABEL == *centos7* ]] ; then
+    export ExtraCMakeOptions="${ExtraCMakeOptions} -Dgcctoolchain=$(dirname $(dirname `which gcc`))"
+  fi
+elif [[ $COMPILER == *native* ]]; then
+  unset CC
+  unset CXX
+  unset FC
+  export ExtraCMakeOptions="-Dhdfs=OFF ${ExtraCMakeOptions}"
+  if [[ $LABEL == *mac* ]]; then
+    export ExtraCMakeOptions="-Dmacos_native=ON -Doracle=OFF ${ExtraCMakeOptions}"
+  fi
+elif [[ $COMPILER == *icc* ]]; then
+  iccyear=2013
+  icc14year=2013
+  icc15year=2015
+  icc16year=2016
+  COMPILERyear=${COMPILER}year
+  iccgcc=4.8
+  icc14gcc=4.8
+  icc15gcc=4.9
+  icc16gcc=4.9
+  GCCversion=${COMPILER}gcc
+  if [ $COMPILER == icc17 ]; then
+      . /cvmfs/projects.cern.ch/intelsw/psxe/linux/17-all-setup.sh
+  elif [ $COMPILER == icc18 ]; then
+      . /cvmfs/projects.cern.ch/intelsw/psxe/linux/18-all-setup.sh
+  else
+      . /afs/cern.ch/sw/lcg/contrib/gcc/${!GCCversion}/${ARCH}-slc6/setup.sh
+      . /afs/cern.ch/sw/IntelSoftware/linux/setup.sh
+      . /afs/cern.ch/sw/IntelSoftware/linux/${ARCH}/xe${!COMPILERyear}/bin/ifortvars.sh intel64
+      . /afs/cern.ch/sw/IntelSoftware/linux/${ARCH}/xe${!COMPILERyear}/bin/iccvars.sh intel64
+  fi
+  export CC=`which icc`
+  export CXX=`which icc`
+  export FC=`which ifort`
+fi
+
+case $ARCH in
+  ppc64le)
+    # The ppc64le build node does not have X11 or GSL installed
+    export ExtraCMakeOptions="${ExtraCMakeOptions} -Dx11=OFF -Dbuiltin_gsl=ON"
+    export ExtraCMakeOptions="${ExtraCMakeOptions} -Dbuiltin_afterimage=OFF"
+    export ExtraCMakeOptions="${ExtraCMakeOptions} -Dasimage=OFF -Dastiff=OFF"
+  ;;
+esac
+
+
+if [[ $LABEL == slc6 || $LABEL == centos7 ]]; then
+    export CCACHE_BASEDIR=/mnt/build/jenkins/workspace/
+    export CCACHE_DIR=/ccache
+    export CCACHE_MAXSIZE=10G
+fi
+
+# Set PYTHON_EXECUTABLE variable if BUILDOPTS is python3
+if [[ "$BUILDOPTS" == "python3" ]]; then
+    export ExtraCMakeOptions="${ExtraCMakeOptions} -DPYTHON_EXECUTABLE=$(which python3)"
+fi
+
+# If run from Jenkins-----------------------------------------------------------------------
+if [ x$WORKSPACE != x ]; then
+  SCRATCH_DIR=$WORKSPACE/ipython
+  export JUPYTER_CONFIG_DIR=$SCRATCH_DIR/.jupyter
+  export JUPYTER_DATA_DIR=$SCRATCH_DIR/.local/share/jupyter
+  export JUPYTER_PATH=$SCRATCH_DIR/.local/share/jupyter
+  export JUPYTER_RUNTIME_DIR=$SCRATCH_DIR/.local/share/jupyter/runtime
+  export IPYTHONDIR=$SCRATCH_DIR/.ipython
+fi
---
+++ jenkins/rootbench/root-build-rootbench.cmake
@@ -0,0 +1,233 @@
+#---Common Setting----------------------------------------------------------
+include(${CTEST_SCRIPT_DIRECTORY}/../rootCommon.cmake)
+
+#---Enable roottest---------------------------------------------------------
+if(CTEST_VERSION STREQUAL "master" OR CTEST_VERSION MATCHES "^v6-")
+  set(testing_options "-Droottest=ON")
+endif()
+
+#---Set TCMalloc for fast builds--------------------------------------------
+if(CTEST_BUILD_CONFIGURATION STREQUAL "Optimized")
+  set(testing_options ${testing_options}" -Dtcmalloc=ON")
+endif()
+
+#---Compose the confguration options----------------------------------------
+set(options -Dall=ON
+            -Dccache=ON
+            -Dtesting=ON
+            ${testing_options}
+            -DCMAKE_INSTALL_PREFIX=${CTEST_INSTALL_DIRECTORY}
+            $ENV{ExtraCMakeOptions})
+
+#---Special build options---------------------------------------------------
+if("$ENV{BUILDOPTS}" STREQUAL "cxx14root7")
+  set(options ${options} -Dcxx14=ON -Droot7=ON)
+endif()
+
+if("$ENV{CXX_VERSION}" STREQUAL "14")
+  set(options ${options} -Dcxx14=ON)
+elseif("$ENV{CXX_VERSION}" STREQUAL "17")
+  set(options ${options} -Dcxx17=ON)
+endif()
+
+if("$ENV{BUILDOPTS}" STREQUAL "cxxmodules" OR
+   "$ENV{BUILDOPTS}" STREQUAL "coverity")
+  unset(CTEST_CHECKOUT_COMMAND)
+endif()
+
+if(CTEST_MODE STREQUAL continuous OR CTEST_MODE STREQUAL pullrequests)
+  find_program(NINJA_EXECUTABLE ninja)
+  if(NINJA_EXECUTABLE)
+    set(CTEST_CMAKE_GENERATOR "Ninja")
+  endif()
+endif()
+
+if ((CMAKE_GENERATOR MATCHES "Visual Studio") AND (CMAKE_GENERATOR_TOOLSET STREQUAL ""))
+  set(options ${options} -Thost=x64)
+endif()
+
+separate_arguments(options)
+
+#----Continuous-----------------------------------------------------------
+if(CTEST_MODE STREQUAL continuous)
+  set(empty $ENV{EMPTY_BINARY})
+  if(empty)
+    #ctest_empty_binary_directory(${CTEST_BINARY_DIRECTORY})
+    file(REMOVE_RECURSE ${CTEST_BINARY_DIRECTORY})
+  else()
+    file(GLOB testruns ${CTEST_BINARY_DIRECTORY}/Testing/*-*)
+    if(testruns)
+      file(REMOVE_RECURSE ${testruns})
+    endif()
+  endif()
+  ctest_start (Continuous TRACK Continuous-${CTEST_VERSION})
+  ctest_update(RETURN_VALUE updates)
+  ctest_configure(BUILD   ${CTEST_BINARY_DIRECTORY}
+                  SOURCE  ${CTEST_SOURCE_DIRECTORY}
+                  OPTIONS "${options}" RETURN_VALUE status)
+  if(NOT ${status} EQUAL 0)
+    message(FATAL_ERROR "Failed to configure project")
+  endif()
+  ctest_read_custom_files(${CTEST_BINARY_DIRECTORY})
+  ctest_build(BUILD ${CTEST_BINARY_DIRECTORY})
+  ctest_submit(PARTS Update Configure Build)
+
+#---Install---------------------------------------------------------------
+elseif(CTEST_MODE STREQUAL install)
+
+  #ctest_empty_binary_directory(${CTEST_BINARY_DIRECTORY})
+  file(REMOVE_RECURSE ${CTEST_BINARY_DIRECTORY})
+  ctest_start(${CTEST_MODE} TRACK Install)
+  ctest_update()
+  ctest_configure(BUILD   ${CTEST_BINARY_DIRECTORY}
+                  SOURCE  ${CTEST_SOURCE_DIRECTORY}
+                  OPTIONS "${options}" APPEND
+                  RETURN_VALUE status)
+  if(NOT ${status} EQUAL 0)
+    message(FATAL_ERROR "Failed to configure project")
+  endif()
+  ctest_read_custom_files(${CTEST_BINARY_DIRECTORY})
+  ctest_build(BUILD ${CTEST_BINARY_DIRECTORY} TARGET install APPEND)
+  ctest_submit(PARTS Update Configure Build)
+  #ctest_empty_binary_directory(${CTEST_BINARY_DIRECTORY})
+  file(REMOVE_RECURSE ${CTEST_BINARY_DIRECTORY})
+
+#---Package---------------------------------------------------------------
+elseif(CTEST_MODE STREQUAL package)
+
+  #ctest_empty_binary_directory(${CTEST_BINARY_DIRECTORY})
+  file(REMOVE_RECURSE ${CTEST_BINARY_DIRECTORY})
+  ctest_start(${CTEST_MODE} TRACK Package)
+  ctest_update()
+  ctest_configure(BUILD   ${CTEST_BINARY_DIRECTORY}
+                  SOURCE  ${CTEST_SOURCE_DIRECTORY}
+                  OPTIONS "${options}" APPEND
+                  RETURN_VALUE status)
+  if(NOT ${status} EQUAL 0)
+    message(FATAL_ERROR "Failed to configure project")
+  endif()
+  ctest_read_custom_files(${CTEST_BINARY_DIRECTORY})
+  ctest_build(BUILD ${CTEST_BINARY_DIRECTORY} TARGET package APPEND)
+  ctest_submit(PARTS Update Configure Build)
+
+#----Pullrequests-----------------------------------------------------------
+elseif(CTEST_MODE STREQUAL pullrequests)
+
+  file(REMOVE_RECURSE ${CTEST_BINARY_DIRECTORY})
+
+  # The code semantically does the following:
+  # 1. Resets the working area (and checks out the ghprbTargetBranch aka master).
+  # 2. git fetch https://github.com/AUTHOR_ID/root.git REMOTE_BRANCH_NAME:LOCAL_BRANCH_NAME
+  # 3. git rebase master
+
+  set(REMOTE_BRANCH_NAME "$ENV{ghprbSourceBranch}")
+  set(LOCAL_BRANCH_NAME "$ENV{ghprbPullAuthorLogin}-$ENV{ghprbSourceBranch}")
+
+  # Use --git-dir as -C isn't available for old git.
+  # FIXME: This can go if we have newer git versions supporting git -C.
+  set(GIT_WORKING_DIR "--git-dir=${REBASE_WORKING_DIR}/.git/ --work-tree=${REBASE_WORKING_DIR}")
+
+  # Clean up the area here. If for some reason the rebase screwed up we do not
+  # need to wait N times the rest of the cleanup procedures to kick in.
+  cleanup_pr_area($ENV{ghprbTargetBranch} ${LOCAL_BRANCH_NAME} ${REBASE_WORKING_DIR})
+
+  execute_process(COMMAND ${CTEST_GIT_COMMAND} fetch $ENV{ghprbAuthorRepoGitUrl} ${REMOTE_BRANCH_NAME}:${LOCAL_BRANCH_NAME} WORKING_DIRECTORY ${REBASE_WORKING_DIR})
+
+  # git rebase master LOCAL_BRANCH_NAME rebases the LOCAL_BRANCH_NAME on master and checks out LOCAL_BRANCH_NAME.
+  # Note that we cannot rebase against origin/master because sometimes (for an unknown to me reason)
+  # origin/master is behind master. It is likely due to the git fetch configuration on the nodes.
+  set(ERROR_OCCURRED 0)
+  execute_process(COMMAND  ${CTEST_GIT_COMMAND} -c user.name=sftnight
+    -c user.email=sftnight@cern.ch rebase -f -v $ENV{ghprbTargetBranch} ${LOCAL_BRANCH_NAME}
+    WORKING_DIRECTORY ${REBASE_WORKING_DIR}
+    TIMEOUT 300
+    RESULT_VARIABLE ERROR_OCCURRED
+    )
+  if (ERROR_OCCURRED)
+    # We are in the error case, switch to master to clean up the created branch.
+    cleanup_pr_area($ENV{ghprbTargetBranch} ${LOCAL_BRANCH_NAME} ${REBASE_WORKING_DIR})
+    message(FATAL_ERROR "Rebase of ${LOCAL_BRANCH_NAME} branch on top of $ENV{ghprbTargetBranch} failed!")
+  endif()
+
+  # A PR can require changes to root.git and roottest.git. Try switching root or roottest
+  # (depending if the PR build was triggered from root or roottest repo) to AUTHOR_ID-BRANCH_NAME if
+  # we have if. This ensures consistent building and testing.
+  set(FETCH_FAILED)
+
+  # Clean up the area of the 'other' repository, too.
+  cleanup_pr_area($ENV{ghprbTargetBranch} ${LOCAL_BRANCH_NAME} ${OTHER_REPO_FOR_BRANCH_SYNC_SOURCE_DIR})
+
+  execute_process(COMMAND ${CTEST_GIT_COMMAND} fetch ${OTHER_REPO_FOR_BRANCH_SYNC_GIT_URL} ${REMOTE_BRANCH_NAME}:${LOCAL_BRANCH_NAME}
+    WORKING_DIRECTORY "${OTHER_REPO_FOR_BRANCH_SYNC_SOURCE_DIR}"
+    RESULT_VARIABLE FETCH_FAILED)
+
+  # If fetch failed this means the user did not have the clone of root/roottest or did not have a branch
+  # with the expected name. Ignore and continue.
+  if(NOT FETCH_FAILED)
+    message(WARNING "Found remote ${OTHER_REPO_FOR_BRANCH_SYNC_GIT_URL} with corresponding branch name ${REMOTE_BRANCH_NAME}. \
+Integrating against it. Please make sure you open and merge a PR against it.")
+    # If we have a corresponding branch, check it out and rebase it as we do for above.
+    # FIXME: Figure out how to factor out the rebase cmake fragments.
+    execute_process(COMMAND  ${CTEST_GIT_COMMAND} checkout -f $ENV{ghprbTargetBranch}
+      WORKING_DIRECTORY ${OTHER_REPO_FOR_BRANCH_SYNC_SOURCE_DIR})
+    execute_process(COMMAND  ${CTEST_GIT_COMMAND} -c user.name=sftnight
+      -c user.email=sftnight@cern.ch rebase -f -v $ENV{ghprbTargetBranch} ${LOCAL_BRANCH_NAME}
+      WORKING_DIRECTORY ${OTHER_REPO_FOR_BRANCH_SYNC_SOURCE_DIR}
+      TIMEOUT 300
+      RESULT_VARIABLE ERROR_OCCURRED
+      )
+    if (ERROR_OCCURRED)
+      cleanup_pr_area($ENV{ghprbTargetBranch} ${LOCAL_BRANCH_NAME} ${OTHER_REPO_FOR_BRANCH_SYNC_SOURCE_DIR})
+      message(FATAL_ERROR "Rebase of ${LOCAL_BRANCH_NAME} branch on top of $ENV{ghprbTargetBranch} in ${WARNING_OTHER_REPO} failed!")
+    endif()
+  endif()
+
+  ctest_start (Pullrequests TRACK Pullrequests)
+
+  # Note that we cannot use CTEST_GIT_UPDATE_CUSTOM to host our rebase command because cdash will
+  # start showing all changes from the PR's HEAD (which can be very old) to current master.
+  # In order to workaround this issue we do the rebase outside of the ctest update system. Then,
+  # we checkout the master branch and then checkout the already rebased branch. This way we trick
+  # ctest_update to pick up only the relevant differences.
+  execute_process(COMMAND  ${CTEST_GIT_COMMAND} checkout -f $ENV{ghprbTargetBranch} WORKING_DIRECTORY ${REBASE_WORKING_DIR})
+
+  # Do not put ${CTEST_GIT_COMMAND} checkout ${LOCAL_BRANCH_NAME} in quotes! It breaks ctest_update.
+  set(CTEST_GIT_UPDATE_CUSTOM ${CTEST_GIT_COMMAND} checkout ${LOCAL_BRANCH_NAME})
+  ctest_update(SOURCE "${REBASE_WORKING_DIR}" RETURN_VALUE updates)
+
+  if(updates LESS 0) # stop if update error
+    # We are in the error case, switch to master to clean up the created branch.
+    cleanup_pr_area($ENV{ghprbTargetBranch} ${LOCAL_BRANCH_NAME} ${REBASE_WORKING_DIR})
+    ctest_submit(PARTS Update)
+    message(FATAL_ERROR "There are no updated files. Perhaps the rebase of ${LOCAL_BRANCH_NAME} branch on top of $ENV{ghprbTargetBranch} failed!")
+  endif()
+  ctest_configure(BUILD   ${CTEST_BINARY_DIRECTORY}
+                  SOURCE  ${CTEST_SOURCE_DIRECTORY}
+                  OPTIONS "${options}" RETURN_VALUE status)
+  if(NOT ${status} EQUAL 0)
+    message(FATAL_ERROR "Failed to configure project")
+  endif()
+  ctest_read_custom_files(${CTEST_BINARY_DIRECTORY})
+  ctest_build(BUILD ${CTEST_BINARY_DIRECTORY})
+  ctest_submit(PARTS Update Configure Build)
+
+  # We must not delete the branches here. They are deleted *after* running ctest (in root-test.cmake).
+
+
+#---Experimental/Nightly----------------------------------------------------
+else()
+
+  #ctest_empty_binary_directory(${CTEST_BINARY_DIRECTORY})
+  file(REMOVE_RECURSE ${CTEST_BINARY_DIRECTORY})
+  ctest_start(${CTEST_MODE})
+  ctest_update(SOURCE ${CTEST_SOURCE_DIRECTORY})
+  ctest_configure(BUILD   ${CTEST_BINARY_DIRECTORY}
+                  SOURCE  ${CTEST_SOURCE_DIRECTORY}
+                  OPTIONS "${options}" RETURN_VALUE status)
+  if(NOT ${status} EQUAL 0)
+    message(FATAL_ERROR "Failed to configure project")
+  endif()
+  ctest_read_custom_files(${CTEST_BINARY_DIRECTORY})
+  ctest_build(BUILD ${CTEST_BINARY_DIRECTORY})
+  ctest_submit(PARTS Update Configure Build)
+endif()
