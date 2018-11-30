1aaf3c7128f4acdabbc03a17717773d4e68c4c0c
Axel Naumann <Axel.Naumann@cern.ch>
2018-11-30 21:17:32 +0100

    Revert "Disable openblas on Fed29, it miscalculates TMVA-DNN-CNN-Backpropagation-CPU."
    This reverts commit 34588a7ded1d0df930305fbc868594b6ae8dc458.
    
    Merged by mistake; we are using "-DBLA_VENDOR=Generic" instead.

--- jenkins/root-build.cmake
+++ jenkins/root-build.cmake
@@ -211,6 +211,7 @@ function(GET_ALL_SUPPORTED_MODULES_LINUX LABEL)
     table
     thread
     tmva
+    tmva-cpu
     tmva-pymva
     tmva-rmva
     unuran
@@ -226,13 +227,6 @@ function(GET_ALL_SUPPORTED_MODULES_LINUX LABEL)
   if(NOT "${LABEL}" MATCHES "clang-HEAD")
     list(APPEND all_supported
       imt
-    )
-  endif()
-
-  # Fedora27's openblas is buggy.
-  if(NOT "${LABEL}" MATCHES "fedora27")
-    list(APPEND all_supported
-      tmva-cpu
     )
   endif()
 
