b56439c64ee8682f24672d5f353a9de6ec6b4863
Axel Naumann <Axel.Naumann@cern.ch>
2020-05-12 17:33:35 +0200

    If enabling assertions, do it also for llvm.

--- jenkins/root-build.cmake
+++ jenkins/root-build.cmake
@@ -775,7 +775,7 @@ function(CONFIGURE_ROOT_OPTIONS)
 
   #---ASSERTS-----------------------------------------------------------------
   if(NOT CTEST_MODE STREQUAL package AND NOT CTEST_BUILD_CONFIGURATION STREQUAL Debug)
-    set(asserts_options "-DCMAKE_CXX_FLAGS=-UNDEBUG")
+    set(asserts_options "-DCMAKE_CXX_FLAGS=-UNDEBUG -DLLVM_ENABLE_ASSERTIONS=On")
   endif()
 
   #---Compose the configuration options---------------------------------------
