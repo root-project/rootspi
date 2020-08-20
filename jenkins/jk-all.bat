f9287b833bdbcd7721c688bfda4cdd4875c23881
Bertrand Bellenot <bertrand.bellenot@cern.ch>
2020-08-20 16:03:09 +0200

    Limit the number of cores (prevent killing the communication with Jenkins)

--- jenkins/jk-all.bat
+++ jenkins/jk-all.bat
@@ -30,6 +30,7 @@ rem ---Run the CTest script depending on the compiler---------------------------
 set THIS=%~d0%~p0
 set NCORES=%NUMBER_OF_PROCESSORS%
 if "!NCORES!" == "" set NCORES=4
+if "!NCORES!" == "32" set NCORES=16
 set ACTION=%1
 set RUN_TESTS=no
 
