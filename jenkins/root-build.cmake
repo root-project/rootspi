6b1ff65c31cf6d030da9ff73476f880cb6843b87
Bertrand Bellenot <bertrand.bellenot@cern.ch>
2020-08-25 16:43:00 +0200

    Come back to the previous version check (should be fixed now)

--- jenkins/root-build.cmake
+++ jenkins/root-build.cmake
@@ -150,7 +150,7 @@ function(GET_ALL_SUPPORTED_MODULES_WIN32)
       tmva
     )
   endif()
-  if(ROOT_VERSION VERSION_EQUAL 6.22)
+  if(ROOT_VERSION VERSION_GREATER 6.21 AND ROOT_VERSION VERSION_LESS 6.23)
     list(APPEND all_supported
       pyroot_legacy
     )
