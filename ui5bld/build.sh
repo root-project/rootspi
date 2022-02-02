691f19814704f0a76d80ba34de655abccdfa04b4
Sergey Linev <S.Linev@gsi.de>
2022-02-02 15:14:41 +0100

    Adjust script for openui5 1.98.0

--- ui5bld/build.sh
+++ ui5bld/build.sh
@@ -4,12 +4,13 @@ rm -rf resources *.txt
 
 src=${1}
 if [[ "$src" == "" ]]; then
-   src=~/d/openui5-1.82.2/
+   src=~/d/openui5-1.98.0/
 fi
 
 copy_lib() {
    echo "copy lib $1"
    mkdir -p ${1}
+   cp ${src}/${1}/library.js ${1}
    cp ${src}/${1}/library-preload.js ${1}
    cp ${src}/${1}/library-preload.js.map ${1}
    cp ${src}/${1}/messagebundle.properties ${1}/messagebundle.properties
@@ -31,12 +32,15 @@ cp ${src}/resources/sap-ui-core.js.map resources/
 
 copy_lib resources/sap/ui/core
 copy_themes resources/sap/ui/core
+mkdir resources/sap/ui/core/cldr
+cp ${src}/resources/sap/ui/core/cldr/en.json resources/sap/ui/core/cldr
 
 # copy_lib resources/sap/ui/codeeditor
 
 mkdir -p resources/sap/ui/codeeditor/
 mkdir -p resources/sap/ui/codeeditor/js/ace/
 mkdir -p resources/sap/ui/codeeditor/js/ace/snippets/
+mkdir -p resources/sap/ui/codeeditor/js/ace/css/
 cp ${src}/resources/sap/ui/codeeditor/library-preload.js resources/sap/ui/codeeditor/
 cp ${src}/resources/sap/ui/codeeditor/library-preload.js.map resources/sap/ui/codeeditor/
 cp ${src}/resources/sap/ui/codeeditor/js/ace/ace.js resources/sap/ui/codeeditor/js/ace/
@@ -46,6 +50,7 @@ cp ${src}/resources/sap/ui/codeeditor/js/ace/mode-c_cpp.js resources/sap/ui/code
 cp ${src}/resources/sap/ui/codeeditor/js/ace/mode-css.js resources/sap/ui/codeeditor/js/ace/
 cp ${src}/resources/sap/ui/codeeditor/js/ace/mode-javascript.js resources/sap/ui/codeeditor/js/ace/
 cp ${src}/resources/sap/ui/codeeditor/js/ace/mode-json.js resources/sap/ui/codeeditor/js/ace/
+cp ${src}/resources/sap/ui/codeeditor/js/ace/mode-html.js resources/sap/ui/codeeditor/js/ace/
 cp ${src}/resources/sap/ui/codeeditor/js/ace/mode-latex.js resources/sap/ui/codeeditor/js/ace/
 cp ${src}/resources/sap/ui/codeeditor/js/ace/mode-markdown.js resources/sap/ui/codeeditor/js/ace/
 cp ${src}/resources/sap/ui/codeeditor/js/ace/mode-plain_text.js resources/sap/ui/codeeditor/js/ace/
@@ -73,6 +78,7 @@ cp ${src}/resources/sap/ui/codeeditor/js/ace/snippets/latex.js resources/sap/ui/
 cp ${src}/resources/sap/ui/codeeditor/js/ace/snippets/css.js resources/sap/ui/codeeditor/js/ace/snippets/
 cp ${src}/resources/sap/ui/codeeditor/js/ace/snippets/sh.js resources/sap/ui/codeeditor/js/ace/snippets/
 cp ${src}/resources/sap/ui/codeeditor/js/ace/snippets/xml.js resources/sap/ui/codeeditor/js/ace/snippets/
+cp -r ${src}/resources/sap/ui/codeeditor/js/ace/css/ resources/sap/ui/codeeditor/js/ace/
 cp ${src}/resources/sap/ui/codeeditor/messagebundle.properties resources/sap/ui/codeeditor/
 cp ${src}/resources/sap/ui/codeeditor/messagebundle_en.properties resources/sap/ui/codeeditor/
 
@@ -87,8 +93,10 @@ copy_lib resources/sap/tnt
 copy_themes resources/sap/tnt
 
 copy_lib resources/sap/uxap
-mv resources/sap/uxap/messagebundle_en.properties resources/sap/uxap/messagebundle_en_US.properties
 copy_themes resources/sap/uxap
+
+copy_lib resources/sap/f
+copy_themes resources/sap/f
 
 copy_lib resources/sap/ui/layout
 copy_themes resources/sap/ui/layout
